b7f18c8c5991f77810ae8ee1920893309b4a564a
Prashant Cholachagudda <pvc@outlook.com>
2019-04-24 16:48:58 +0530

    added the client id

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,16 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { MsalModule } from "@azure/msal-angular";
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    MsalModule.forRoot({
+      clientID: "7427b397-d6a7-4b1c-b9ed-a1c9df39d448"
+    })
+  ],
   providers: [
     StatusBar,
     SplashScreen,
@@ -20,4 +26,4 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
